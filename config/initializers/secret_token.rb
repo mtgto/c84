f100d3e0fa225d49ebbab839d5ba9181bf79903d
mtgto <hogerappa@gmail.com>
2013-06-17 22:31:44 +0900

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Twostepauth::Application.config.secret_key_base = 'c4eb48fc7018f23a764ac75e809e6f863a0be2587161978decf50d308188afdac1b044d6120a3b5adbd4765a205cc16c6894daf3114eff07931d144e4fedd840'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Twostepauth::Application.config.session_store :cookie_store, key: '_twostepauth_session'
